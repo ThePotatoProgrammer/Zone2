514069bcf65c9456a0c7201b669190b5098612fa
Joshua Baker <thepotatoprogrammer@gmail.com>
2022-05-28 22:33:51 -0500

    Initial Commit

---
+++ Zone2/Zone2App.swift
@@ -0,0 +1,17 @@
+//
+//  Zone2App.swift
+//  Zone2
+//
+//  Created by Joshua Baker on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Zone2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
